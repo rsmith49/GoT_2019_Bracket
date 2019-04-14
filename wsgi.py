2fb9d9b8b31b30144d5a30489dbe7bf9f39f50f1
rsmith49 <ryans_cais@yahoo.com>
2019-04-14 16:04:29 -0700

    Added showing individual brackets

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 
-from src.bracket import find_all_brackets, Bracket
+from src.bracket import find_all_brackets, Bracket, DEFAULT_BRACKET_PATH
 from src.char_pred import PRED_TYPES
 from src.show_state import get_curr_show_state
 from src.utils import CHAR_TIERS, TIERS, CHAR_NAMES
@@ -17,20 +17,16 @@ def home():
 
 @application.route('/results/')
 def list_bracket_results():
-    bracket_results = [
-        {
-            'rank': 'Place',
-            'bracket_name': 'Bracket Name',
-            'points': 'Points',
-            'potential_points': 'Potential Points'
-        }
-    ]
+    bracket_results = []
 
     brackets = find_all_brackets()
     state = get_curr_show_state()
 
     rank = 1
-    for bracket in sorted(brackets, key=lambda bracket: -bracket.points(state)):
+    for bracket in sorted(brackets, key=lambda bracket: (
+            -bracket.points(state),
+            -bracket.potential_points(state)
+    )):
         bracket_results.append({
             'rank': rank,
             'bracket_name': bracket.name,
@@ -79,6 +75,44 @@ def create_bracket():
         'bracket_created.html',
         message=message
     )
+
+
+@application.route('/brackets/<bracket_name>')
+def show_bracket(bracket_name):
+    bracket = Bracket(
+        filename=DEFAULT_BRACKET_PATH + bracket_name.replace(' ', '_') + '_bracket.json'
+    )
+    show_state = get_curr_show_state()
+
+    char_preds = sorted(
+        [
+            {
+                'char_name': char_name,
+                'char_tier': bracket.char_preds[char_name].char_tier,
+                'pred_type': bracket.char_preds[char_name].pred_type,
+                'pred_week': bracket.char_preds[char_name].pred_val,
+                'points': bracket.char_preds[char_name].points(
+                    show_state.state[char_name]
+                ),
+                'potential_points': bracket.char_preds[char_name].potential_points(
+                    show_state.state[char_name]
+                )
+            }
+            for char_name in bracket.char_preds
+        ],
+        key=lambda x: (x['char_tier'], x['char_name'])
+    )
+
+    return render_template(
+        'bracket_show.html',
+        bracket_name=bracket.name,
+        char_preds=char_preds
+    )
+
+
+@application.route('/update_show_state', methods=['POST'])
+def update_show_state():
+    pass
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port='8080')
